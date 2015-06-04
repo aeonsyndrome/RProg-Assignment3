656263de610292f0984226022332e49423d3999f
lduplay <leon.duplay@gmail.com>
2015-06-04 15:15:30 +0200

    Finished best.R

--- best.R
+++ best.R
@@ -5,13 +5,25 @@
 best <- function(state, outcome) {
   ## Read outcome data
   data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  validoutcome <- c("heart attack", "heart failure", "pneumonia")
   
   ## Check that state and outcome are valid
-  if (!is.element(outcome,validoutcome)) stop("invalid outcome")
   if (!is.element(state,data$State)) stop("invalid state")
-  
+  validoutcome <- c("heart attack", "heart failure", "pneumonia")
+  if (!is.element(outcome,validoutcome)) stop("invalid outcome")
   
   ## Return hospital name in that state with lowest 30-day death
+  data <- data[data[,"State"]==state,] #Select only data in the right state
+  outcomecolumn <- switch(outcome,
+      "heart attack" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack",
+      "heart failure" = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure",
+      "pneumonia" = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+  )
+  
+  ## Clean data
+  data <- data[,c("Hospital.Name",outcomecolumn)]
+  data[,2] <- suppressWarnings(as.numeric(data[,2]))
+  
   ## rate
+  bestidx <- which.min(data[,2])
+  data[bestidx,]$Hospital.Name
 }
